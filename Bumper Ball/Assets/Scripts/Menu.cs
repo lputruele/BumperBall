c8f45a2e24e4ff2b8d6648418eb5426a96b41771
Luciano Putruele <lputruele@gmail.com>
2016-09-28 21:21:12 -0300

    Bumper Ball Unity Game - first commit

---
+++ Bumper Ball/Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Canvas tempMenu;
+	// Use this for initialization
+	void Start () {
+		if (tempMenu != null) {
+			tempMenu = tempMenu.GetComponent<Canvas> ();
+			tempMenu.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	public void PressPlay(){
+		Application.LoadLevel ("scene1");
+	}
+
+	public void PressQuit(){
+		Application.Quit ();
+	}
+
+	public void PressBack(){
+		Application.LoadLevel ("Menu");
+	}
+
+	public void EnableGameOverMenu(){
+		tempMenu.enabled = true;
+	}
+
+}
---
+++ Bumper Ball/Assets/Scripts/Player.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public bool isHuman;
+	public GameObject[] otherPlayers;
+	private static float moveSpeed = 10;
+	private GameObject target; //for IA only
+	private Rigidbody body;
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+	private GameObject ground;
+	private bool hasLost;
+	public GameObject menu;
+	private Vector2 touchStart;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody> ();
+		ground = GameObject.Find("Ground");
+		if (!isHuman) { // is a bot
+			ChooseTarget ();
+			InvokeRepeating ("ChooseTarget", 0, 5); //decide a target every 5 seconds
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int i;
+		//check if player is losing
+		if (Vector3.Distance (this.transform.position, ground.transform.position) >= 8) {
+			//Debug.Log("Game over " + this.transform.name);
+			hasLost = true;
+			if (isHuman && menu != null){
+				menu.GetComponent<Menu>().EnableGameOverMenu();
+			}
+		}
+		//check if player is winning
+		for (i=0; i< otherPlayers.Length; i++) { 
+			if (!otherPlayers[i].GetComponent<Player>().GetHasLost())
+				break;
+		}
+		if (i == otherPlayers.Length && !hasLost && menu != null) {
+			//Debug.Log (this.transform.name + "WINS ");
+			menu.GetComponent<Menu>().EnableGameOverMenu();
+		}
+	}
+
+	// Fixed update for physics i.e movement
+	void FixedUpdate () {
+		if (!hasLost) {
+			if (isHuman) {
+				moveInput = Vector3.zero;
+				if (Input.touchCount > 0){
+					switch (Input.GetTouch(0).phase){
+					case TouchPhase.Began:
+						touchStart = Input.GetTouch(0).position;
+						break;
+					case TouchPhase.Moved:
+						Vector2 dir = Input.GetTouch(0).position - touchStart;
+						moveInput = new Vector3(dir.x,0f,dir.y);
+						break;
+					default:
+						break;
+					}
+				}
+				//moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0f, Input.GetAxisRaw ("Vertical"));
+			} else {
+				if (Vector3.Distance (this.transform.position, ground.transform.position) >= 6) { //close to falling!
+					moveInput = ground.transform.position - this.transform.position;
+				} else {
+					moveInput = target.transform.position - this.transform.position;
+				}
+			}
+			moveInput.Normalize ();
+			moveVelocity = moveInput * moveSpeed;
+			body.AddForce (moveVelocity);
+		}
+	}
+
+	//When colliding with another player there is a knockback relative to the other's velocity
+	void OnCollisionEnter(Collision c) {
+		if (c.rigidbody != null) { //it's not the ground
+			Vector3 dir = c.contacts[0].point - transform.position; //get angle between the collision point and the player
+			float force = c.relativeVelocity.magnitude * 8;
+			dir = -dir.normalized; //get the opposite vector and normalize it
+			body.AddForce(dir*force); //push back the player
+		}
+	}
+
+	//This one is to make sure players always bounce from each other
+	void OnCollisionStay(Collision c){
+		if (c.rigidbody != null) { //it's not the ground
+			Vector3 dir = c.contacts[0].point - transform.position;
+			float force = c.relativeVelocity.magnitude * 3;
+			dir = -dir.normalized; 
+			body.AddForce(dir*force);
+		}
+	}
+
+	//IA function that chooses a target player to bump into
+	void ChooseTarget (){
+		int r = Random.Range (0, 3);
+		if (!otherPlayers[r].GetComponent<Player>().GetHasLost())
+			target = otherPlayers[r];
+	}
+
+	public bool GetHasLost(){
+		return hasLost;
+	}
+
+	public Rigidbody GetBody(){
+		return body;
+	}
+
+	public Vector3 GetMoveVelocity(){
+		return moveVelocity;
+	}
+	
+}
